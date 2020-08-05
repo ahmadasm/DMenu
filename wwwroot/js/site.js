4abe068839577e3b7fa490e420a60a35e6279c15
ahmad <ahmad.hosseini450@gmail.com>
2020-08-05 16:46:53 +0430

    config nestedSortable

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,21 @@
 ﻿$(document).ready(function(){
-   
+    var ns = $('ol.sortable').nestedSortable({
+        forcePlaceholderSize: true,
+        handle: 'div',
+        helper:	'clone',
+        items: 'li',
+        opacity: .6,
+        placeholder: 'placeholder',
+        revert: 250,
+        tabSize: 25,
+        tolerance: 'pointer',
+        toleranceElement: '> div',
+        maxLevels: 4,
+        isTree: true,
+        expandOnHover: 700,
+        startCollapsed: false,
+        change: function(){
+            console.log('Relocated item');
+        }
+    });
 });
