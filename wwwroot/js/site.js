acefcba9cf5bd0801ab5f067cb725bb1fbf123e7
ahmad <ahmad.hosseini450@gmail.com>
2020-08-07 14:08:33 +0430

    change title and name of menu item by changing position

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -17,6 +17,30 @@
         rtl: true,
         change: function(){
             console.log('Relocated item');
+        },
+        start: function(e, ui) {
+            // creates a temporary attribute on the element with the old index
+            $(this).attr('data-previndex', ui.item.index());
+        },
+        update: function(e, ui) {
+        },
+        stop: function(event, ui) {
+            var menuItems = $('li.webo-menu-item');
+            for(var i = 0; i < menuItems.length;i++)
+            {
+                SetHeader(menuItems[i],i);
+                //alert(i + " : " + menuItems[i]);
+            }
+            
         }
     });
+    function SetHeader(menuItem,index)
+    {
+        var title = $(menuItem).find('.panel-title a');
+        title.text(index);
+        var nameInput = $(menuItem).find('.menu-item-name');
+        var newName = 'Names[' + index + ']';
+        nameInput.attr('name',newName);
+        //nameInput.val(index);
+    }
 });
