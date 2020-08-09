7c78cfd02a8c4fe90409cec72d914c96c31a5060
ahmad <ahmad.hosseini450@gmail.com>
2020-08-09 10:32:04 +0430

    add css class field into editable menu

--- ViewModels/EditableMenu.cs
+++ ViewModels/EditableMenu.cs
@@ -7,5 +7,6 @@ namespace MenuTest.ViewModels
         public List<string> Parents { get; set; }
         public List<string> Text { get; set; }
         public List<string> Links { get; set; }
+        public List<string> CssClasses { get; set; }
     }
 }
