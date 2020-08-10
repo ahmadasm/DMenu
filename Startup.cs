fa0a98272a9f38e6e683a021211246c5b632bed4
ahmad <ahmad.hosseini450@gmail.com>
2020-08-10 11:15:24 +0430

    register menu db context in services

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.EntityFrameworkCore;
+using MenuTest.Dal;
 namespace MenuTest
 {
     public class Startup
@@ -23,6 +19,8 @@ namespace MenuTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MenuDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MenuDbContext")));
             services.AddControllersWithViews();
         }
 
