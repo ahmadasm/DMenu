c3132727fab1a4cf96ee92b2ecb7ecadf4318f5e
ahmad <ahmad.hosseini450@gmail.com>
2020-08-10 11:13:07 +0430

    add menu db context class

---
+++ DAL/MenuDbContext.cs
@@ -0,0 +1,14 @@
+using System;
+using MenuTest.Models;
+using Microsoft.EntityFrameworkCore;
+namespace MenuTest.Dal
+{
+    public class MenuDbContext:DbContext
+    {
+        public MenuDbContext(DbContextOptions<MenuDbContext> options)
+            :base(options)
+        {}
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
+    }
+}
