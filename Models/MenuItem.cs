424238c71d793203f4ea1553dbe956cc3269be51
ahmad <ahmad.hosseini450@gmail.com>
2020-08-11 10:25:00 +0430

    add creation date to menu item model

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -5,6 +5,7 @@ namespace MenuTest.Models
     public class MenuItem
     {
         public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
         public string Text { get; set; }
         public string Href { get; set; }
         public bool OpenInNewTab { get; set; }
